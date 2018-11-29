654af5b20f3121149b3a3185ece163c85177a90d
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-11-28 23:26:50 -0800

    min app 2 - form data available on the server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,23 @@
 
+$('form').on('submit', function(e){
+  e.preventDefault();
+
+  var data = $('form').serializeArray().reduce(function(obj, item) {
+    obj[item.name] = item.value;
+    return obj;
+  }, {});
+
+  var message = data.jsontext;
+  //console.log('my message pre-ajax: ', message);
+
+  /* initiate ajax */
+  $.ajax({
+    url: "http://127.0.0.1:3001/json",
+    type: 'POST',
+    data: message,
+    contentType: 'application/json',
+    success: function(result){
+    $("#jsonText").html(result);
+  }});
+});
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,18 +1,31 @@
-var express = require('express');
-var http = require('http');
-var app = express();
+const express = require('express');
+const http = require('http');
+var bodyParser = require('body-parser');
+const app = express();
 
-var port = 3001;
+const port = 3001;
+// var ip = '127.0.0.1';
 
 http.createServer(app).listen(port, function () {
   console.log('Listening on Port ' + port);
 });
 
+/* index.html */
+app.use(express.static('client'));
+// create application/json parser
+app.use(bodyParser.json());
+
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
 /* routes */
-app.get('/', function (req, res) {
-  res.send('GET request from the homepage')
+app.post('/json', function (req, res) {
+  console.log('data on server:',  {message: req.body.children});
+  res.status(200).send('POST request to the server');
+  res.end();
 })
 
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
+app.get('/', function (req, res) {
+  res.status(200).send('GET request from the homepage');
 })
+
