c87e579f665462ec382d81849aea105469033efd
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-11-28 20:25:21 -0800

    app 2 basic server set up  and html page set up

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+var http = require('http');
+var app = express();
 
+var port = 3001;
 
+http.createServer(app).listen(port, function () {
+  console.log('Listening on Port ' + port);
+});
+
+/* routes */
+app.get('/', function (req, res) {
+  res.send('GET request from the homepage')
+})
+
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
