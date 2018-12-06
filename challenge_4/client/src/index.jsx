cbad5c0844319788f5c6eafc1eeae4f42cb9891d
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-12-05 18:03:43 -0800

    setup complete

---
+++ challenge_4/client/src/index.jsx
@@ -0,0 +1,17 @@
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <div>
+        what up ?
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
 
+const app = express();
+const port = 3001;
 
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('./client/dist/'));
+
+app.listen(port, () => {
+  console.log(`Server is now running on ${port}`);
+});
