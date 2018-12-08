3ef30d5e366fa9cc4be9ea45b5d92de874fbd3ae
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-12-07 22:07:06 -0800

    vertical and horizontal win checking complete

--- challenge_4/client/src/components/square.jsx
+++ challenge_4/client/src/components/square.jsx
@@ -1,67 +1,33 @@
 export default class Square extends React.Component{
   constructor(props){
     super(props);
-    // this.state = {
-    //  play: ''
-    // }
   }
 
   componentDidMount(){
-    console.log('componentDidMount ran');
-    //this.props.setPiece();
+    //console.log('componentDidMount ran');
   }
 
   sqrClicked(){
-    //console.log('props in the onclick Handlder in sqr', this.props);
    this.props.handleClick(this.props.row, this.props.col)
   }
 
-  // setPiece(){
-  //   // console.log('my board :', this.props.board);
-  //   // console.log('my board0 pos:', this.props.board[0][0]);
-  //    var row = this.props.row;
-  //    var col = this.props.col;
-  //   // console.log('my row: ', row);
-  //   // console.log('my coll: ', coll);
-  //   // console.log('my board x and y pos:', this.props.board[row][coll]);
-  //  // debugger;
-
-  //   if(this.props.board[Number(row)][Number(col)] === 1){
-  //    // console.log('Red: ', this.props.board[Number(row)][Number(col)]);
-  //     this.setState({
-  //       play: 'red'
-  //     })
-  //   //  console.log(this.state.play);
-  //   }
-  //   if (this.props.board[Number(row)][Number(col)] === 0) {
-  //   //  console.log('Yellow: ', this.props.board[Number(row)][Number(col)]);
-  //     this.setState({
-  //       play: 'yel'
-  //     })
-  //    // console.log(this.state.play);
-  //   }
-
-  // }
 
   render () {
     if(this.props.board[this.props.row][this.props.col] === 1 ){
       return (
         <div className="sqr" onClick={this.sqrClicked.bind(this)} >
-          { console.log('my props inside the component: ', this.props.board[this.props.row][this.props.col]) }
           <div className="red" data-x={this.props.row} data-y={this.props.col}/>
         </div>
       )
     } else if (this.props.board[this.props.row][this.props.col] === 0 ) {
       return (
         <div className="sqr" onClick={this.sqrClicked.bind(this)} >
-          { console.log('my props inside the component: ', this.props.board[this.props.row][this.props.col]) }
           <div className="yel" data-x={this.props.row} data-y={this.props.col}/>
         </div>
       )
     } else {
         return (
         <div className="sqr" onClick={this.sqrClicked.bind(this)} >
-          { console.log('my props inside the component: ', this.props.board[this.props.row][this.props.col]) }
           <div className="" data-x={this.props.row} data-y={this.props.col}/>
         </div>
         )
--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -25,11 +25,17 @@ class App extends React.Component{
   }
 
   playRound() {
-    console.log('current row/col: ', this.state.currRow, ',', this.state.currCol);
-    this.isEmpty();
-    this.changeColor();
-
-    // check how many in a row / col / left diagonal / right diagonal
+    if(!this.state.win){
+      console.log('current row/col: ', this.state.currRow, ',', this.state.currCol);
+      this.isEmpty();
+      // check how many in a row / col / left diagonal / right diagonal
+      this.checkHorizontal();
+      this.checkVertical();
+      this.checkLeftDiagonal();
+      //   this.checkRightDiagonal();
+      // change color
+        this.changeColor();
+    }
   }
 
   isEmpty() {
@@ -39,7 +45,9 @@ class App extends React.Component{
         newBoard[i].splice(this.state.currCol, 1, this.state.color);
         this.setState({
           board: newBoard
-        }), () => console.log('state set on new board', this.state.board);
+          })
+         // }), () => console.log('state set on board', this.state.board);
+         // debugger;
         return;
       }
     }
@@ -56,6 +64,199 @@ class App extends React.Component{
       }), () => console.log('color', this.state.color, 'play:', this.state.play);
     }
   }
+
+  winh() {
+      if(this.state.color === 1){
+        console.log('YELLOW WINS Horizontally !! ');
+      } else if(this.state.color === 0){
+        console.log('RED WINS Horizontally !! ');
+      }
+  }
+
+  winv() {
+    if(this.state.color === 1){
+      console.log('YELLOW WINS Vertically !! ');
+    } else if(this.state.color === 0){
+      console.log('RED WINS Vertically!! ');
+    }
+  }
+
+  windtlr() {
+    if(this.state.color === 1){
+      console.log('YELLOW WINS Diagonally top left - right !! ');
+    } else if(this.state.color === 0){
+      console.log('RED WINS Diagonally top left - right!! ');
+    }
+  }
+
+  windtrl() {
+    if(this.state.color === 1){
+      console.log('YELLOW WINS Diagonally top right - left !! ');
+    } else if(this.state.color === 0){
+      console.log('RED WINS Diagonally top right - left!! ');
+    }
+  }
+
+
+  checkHorizontal() {
+    let streak = 0;
+    // REDS
+    for (let i = 0; i < this.state.board.length; i++){
+      streak = 0;
+     // console.log('reset - end of line');
+      for (let j = 0; j < this.state.board[i].length; j++){
+        if(this.state.board[i][j] === 1){
+          streak = streak +1;
+      //    console.log('streak: ', streak);
+          if(streak === 4){
+            this.setState({
+              win: true
+            }, () => this.winh());
+          }
+        }
+        if(this.state.board[i][j] === 0 || this.state.board[i][j] === null ){
+          streak = 0;
+        }
+      }
+    }
+    // YELLOWS
+    for (let i = 0; i < this.state.board.length; i++){
+      streak = 0;
+     // console.log('reset - end of line');
+      for (let j = 0; j < this.state.board[i].length; j++){
+        if(this.state.board[i][j] === 0){
+          streak = streak +1;
+      //    console.log('streak: ', streak);
+          if(streak === 4){
+            this.setState({
+              win: true
+            }, () => this.winh());
+          }
+        }
+        if(this.state.board[i][j] === 1 || this.state.board[i][j] === null){
+          streak = 0;
+        }
+      }
+    }
+  }
+  checkVertical() {
+    let streak = 0;
+    // REDS //////// -------------->>
+    for(let i = 0; i < this.state.board[0].length; i++){
+    //  console.log(i);
+      //arrayColumn(board, i);
+    //  console.log('Checking column');
+      var that = this;
+      this.state.board.map(function(x, index){
+        // console.log('that: ', that);
+        // console.log('i', i);
+        // console.log('index', index);
+        // console.log('board', that.state.board[index][i]);
+        if(that.state.board[index][i] === 1){
+      //    console.log('it\'s a red!!');
+          streak = streak +1;
+      //    console.log('streak: ', streak);
+          if(streak === 4){
+            that.setState({
+              win: true
+            }, () => that.winv());
+          }
+        }
+        if(that.state.board[index][i] === 0){
+      //    console.log('it\'s yellow!!');
+          streak = 0;
+     //     console.log('streak: ', streak);
+        }
+      });
+     streak = 0;
+    // console.log('reset - end of column');
+    }
+
+    // YELLOWS //////// -------------->>
+    for(let i = 0; i < this.state.board[0].length; i++){
+      // console.log(i);
+      // console.log('Checking column');
+      var that = this;
+      this.state.board.map(function(x, index){
+        // console.log('that: ', that);
+        // console.log('i', i);
+        // console.log('index', index);
+        // console.log('board', that.state.board[index][i]);
+        if(that.state.board[index][i] === 0){
+      //    console.log('it\'s a yellow!!');
+          streak = streak +1;
+      //    console.log('streak: ', streak);
+          if(streak === 4){
+            that.setState({
+              win: true
+            }, () => that.winv());
+          }
+        }
+        if(that.state.board[index][i] === 1){
+       //   console.log('it\'s red!!');
+          streak = 0;
+       //   console.log('streak: ', streak);
+        }
+      });
+     streak = 0;
+   //  console.log('reset - end of column');
+    }
+
+  }
+
+  checkLeftDiagonal() {
+    // console.log('checkLeftDiagonal running ---> ');
+    // let streak = 0;
+    // let offset = 0;
+    // // REDS
+
+
+
+        /*
+           if row is 2 col 0 depth is 4
+           if row is 1 col 0 depth is 5
+           if row is 0 col 0 depth is 6
+           if row is 0 col 1 depth is 6
+           if row is 0 col 2 depth is 5
+           if row is 0 col 3 depth is 4
+
+           if starting row > 0
+           depth = board.length -row;
+
+           if starting col > 0
+           depth = board[0].length - col;
+
+
+        */
+
+        // if(offset < 4){
+        //   console.log('row',i +offset++);
+        //   console.log('column',j);
+        // }
+      //   if(this.state.board[i][j] === 1){
+      //     streak = streak +1;
+      // //    console.log('streak: ', streak);
+      //     if(streak === 4){
+      //       this.setState({
+      //         win: true
+      //       }, () => this.windtlr());
+      //     }
+      //   }
+      //   if(this.state.board[i][j] === 0){
+      //     streak = 0;
+      //   }
+      //   if(i < 6){
+      //     i = i + 1;
+      //   }
+     // }
+    //}
+  }
+
+
+  checkRightDiagonal() {
+
+  }
+
 
 
   handleClick(row, col) {
